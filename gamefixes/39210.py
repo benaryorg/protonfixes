a06cbe95eb337662f68eaea6f31d397e6dd4e5cb
Thai Chung <github@0xreki.de>
2022-03-14 14:42:46 +0100

    Add option to launch XIVLauncher
    Using protontricks to install requirements for XIVLauncher. XIVLauncher still needs to be installed with protontricks 39210 run

--- gamefixes/39210.py
+++ gamefixes/39210.py
@@ -11,3 +11,10 @@ def main():
     # Fixes the startup process.
     if 'NOSTEAM' in os.environ:
         util.replace_command('-issteam', '')
+    
+    # Runs XIVLauncher instead of Stock Launcher
+    if 'XL_WINEONLINUX' in os.environ:
+        util.protontricks_proton_5('dotnet48')
+        util.protontricks('vcrun2019')
+        util.replace_command('common/FINAL FANTASY XIV Online/boot/ffxivboot.exe', 'compatdata/39210/pfx/drive_c/users/steamuser/AppData/Local/XIVLauncher/XIVLauncher.exe')
+        util.replace_command('-issteam', '')
