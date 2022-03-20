9aecb37eb82bb238b50b5a8667e03906fb70da99
Thai Chung <mail@0xreki.de>
2022-03-20 03:46:34 +0100

    Add environment for better configuration.

--- gamefixes/39210.py
+++ gamefixes/39210.py
@@ -14,6 +14,7 @@ def main():
     
     # Runs XIVLauncher instead of Stock Launcher
     if 'XL_WINEONLINUX' in os.environ:
+        util.set_environment('PROTON_SET_GAME_DRIVE', '1')
         util.protontricks_proton_5('dotnet48')
         util.protontricks('vcrun2019')
         util.replace_command('common/FINAL FANTASY XIV Online/boot/ffxivboot.exe', 'compatdata/39210/pfx/drive_c/users/steamuser/AppData/Local/XIVLauncher/XIVLauncher.exe')
