a10ea17e4e8bc23db123942032a4ce22010ced49
benaryorg <binary@benary.org>
2022-05-01 23:41:50 +0000

    ffxiv launcher: copy changes to demo appid
    Signed-off-by: benaryorg <binary@benary.org>

--- gamefixes/312060.py
+++ gamefixes/312060.py
@@ -11,3 +11,12 @@ def main():
     # Fixes the startup process.
     if 'NOSTEAM' in os.environ:
         util.replace_command('-issteam', '')
+
+    # Runs XIVLauncher instead of Stock Launcher
+    if 'XL_WINEONLINUX' in os.environ:
+        util.set_environment('PROTON_SET_GAME_DRIVE', '1')
+        util.protontricks_proton_5('dotnet48')
+        util.protontricks('vcrun2019')
+        util.protontricks('ffxivlauncher_ge')
+        util.replace_command('common/FINAL FANTASY XIV Online/boot/ffxivboot.exe', 'compatdata/312060/pfx/drive_c/users/steamuser/AppData/Local/XIVLauncher/XIVLauncher.exe')
+        util.replace_command('-issteam', '')
